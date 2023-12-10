d16d75641e9f21e4c8d00d75aaa12317920b5260
juliaeu <juliabenedic13@gmail.com>
2023-12-10 02:13:04 +0000

    "product/purchase dataframe"

--- pandas_django/src/products/views.py
+++ pandas_django/src/products/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
 import pandas as pd
-from .models import Product
+from .models import Product, Purchase
 
 # Create your views here
 # Define a view function to render a chart content
 def render_chart_view(request):
-    # take the data from the database and put it in a dataframe and pass it as a context
-    queriy1 = Product.objects.all().values()
-    print(queriy1)
+    # fetch all product objects from the database and convert them into a pandas DataFrame
+    product_dataframe = pd.DataFrame(Product.objects.all().values())
+    # fetch all the purchase objects from the database and convert them 
+    # into a pandas DataFrame
+    purchase_dataframe = pd.DataFrame(Purchase.objects.all().values())
+
+
+    # create a context dictionary containing the product data in html format
+    context = {
+        'products' : product_dataframe.to_html(),
+        'purchase':purchase_dataframe.to_html(),
+
+    }
     # Render products/main.html template with an empty context
-    return render(request, 'products/main.html', {})
+    return render(request, 'products/main.html', context)
