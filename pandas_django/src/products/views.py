97e1a54e21adda5545be059893c77cbb104612df
juliaeu <juliabenedic13@gmail.com>
2023-12-10 01:03:24 +0000

    product view

--- pandas_django/src/products/views.py
+++ pandas_django/src/products/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here
+# Define a view function to render a chart content
+def render_chart_view(request):
+    # Render products/main.html template with an empty context
+    return render(request, 'products/main.html', {})
