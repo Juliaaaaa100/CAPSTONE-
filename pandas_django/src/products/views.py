a2956cdd759208d3937dd9e55a49d87704530576
juliaeu <juliabenedic13@gmail.com>
2023-12-10 01:49:53 +0000

    views query

--- pandas_django/src/products/views.py
+++ pandas_django/src/products/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+import pandas as pd
+from .models import Product
 
 # Create your views here
 # Define a view function to render a chart content
 def render_chart_view(request):
+    # take the data from the database and put it in a dataframe and pass it as a context
+    queriy1 = Product.objects.all().values()
+    print(queriy1)
     # Render products/main.html template with an empty context
     return render(request, 'products/main.html', {})
