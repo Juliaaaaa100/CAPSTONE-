fa9f3cb0725324a849720570294ba73fa1998c09
juliaeu <juliabenedic13@gmail.com>
2023-12-10 03:01:05 +0000

    adding the static files

--- pandas_django/src/products/views.py
+++ pandas_django/src/products/views.py
@@ -7,15 +7,18 @@ from .models import Product, Purchase
 def render_chart_view(request):
     # fetch all product objects from the database and convert them into a pandas DataFrame
     product_dataframe = pd.DataFrame(Product.objects.all().values())
-    # fetch all the purchase objects from the database and convert them 
-    # into a pandas DataFrame
+    # fetch all the purchase objects from the database and convert them into a pandas DataFrame
     purchase_dataframe = pd.DataFrame(Purchase.objects.all().values())
+    product_dataframe['product_id'] = product_dataframe['id']
+    dataframe_merged = pd.merge(purchase_dataframe, product_dataframe, on='product_id').drop(['date_y', 'id_y'], axis=1).rename({'date_x':'purchase date','id_x':'id'}, axis=1)
+
 
 
     # create a context dictionary containing the product data in html format
     context = {
         'products' : product_dataframe.to_html(),
         'purchase':purchase_dataframe.to_html(),
+        'dataframes_merged':dataframe_merged.to_html(), 
 
     }
     # Render products/main.html template with an empty context
