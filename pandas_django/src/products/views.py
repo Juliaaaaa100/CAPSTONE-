5a711983fb3dbb139241358bb6adfd0180e58d5f
juliaeu <juliabenedic13@gmail.com>
2023-12-13 01:50:59 +0000

    error message and price statistics

--- pandas_django/src/products/views.py
+++ pandas_django/src/products/views.py
@@ -6,8 +6,10 @@ from .utils import fetch_graph
 # Create your views here
 # Define a view function to render a chart content
 def render_chart_view(request):
-    display_error_message= None
-    dataframe_merged= None
+    graph = None
+    display_error_message = None
+    dataframe_merged = None
+    price = None
     # fetch all product objects from the database and convert them into a pandas DataFrame
     product_dataframe = pd.DataFrame(Product.objects.all().values())
     # fetch all the purchase objects from the database and convert them into a pandas DataFrame
@@ -15,7 +17,8 @@ def render_chart_view(request):
     product_dataframe['product_id'] = product_dataframe['id']
  
     if purchase_dataframe.shape[0] > 0:
-        dataframe_merged = pd.merge(purchase_dataframe, product_dataframe, on='product_id').drop(['date_y', 'id_y'], axis=1).rename({'date_x':'purchase date','id_x':'id'}, axis=1)
+        dataframe_merged = pd.merge(purchase_dataframe, product_dataframe, on='product_id').drop(['date_y', 'id_y'], axis=1).rename({'date_x': 'date', 'id_x': 'id'}, axis=1)
+        price = dataframe_merged['price']
         if request.method == 'POST':
             chart_type = request.POST.get('sales')
             date_from = request.POST.get('date_from')
@@ -23,25 +26,24 @@ def render_chart_view(request):
 
             print(chart_type)
 
-            dataframe_merged['purchase date'] = dataframe_merged['purchase date'].apply( lambda x: x.strftime('%Y-%m-%d'))
-            dataframe_merged2 = dataframe_merged.groupby('purchase date', as_index=False)['total_price'].agg('sum')
+            dataframe_merged['date'] = dataframe_merged['date'].apply(lambda x: x.strftime('%Y-%m-%d'))
+            dataframe_merged2 = dataframe_merged.groupby('date', as_index=False)['total_price'].agg('sum')
 
             if chart_type != "":
                 if date_from != "" and date_to != "":
-                    dataframe_merged = dataframe_merged[(dataframe_merged['purchase date']>date_from) & (dataframe_merged['purchase date']< date_to)]
-                    dataframe_merged2 = dataframe_merged.groupby('purchase date', as_index=False)['total_price'].agg('sum')
-                # fetch_graph(chart_type, x=, y=, data =, dataframe=)     
+                    dataframe_merged = dataframe_merged[(dataframe_merged['date'] > date_from) & (dataframe_merged['date'] < date_to)]
+                    dataframe_merged2 = dataframe_merged.groupby('date', as_index=False)['total_price'].agg('sum')
+                graph = fetch_graph(chart_type, x=dataframe_merged2['date'], y=dataframe_merged2['total_price'], data=dataframe_merged)
             else:
                 display_error_message = "In order to continue, select a chart type"
-
-    else:
-        display_error_message = "Sorry, no records have been found in the database"
-
+        else:
+            display_error_message = "Sorry, no records have been found in the database"
 
     # create a context dictionary containing the product data in html format
     context = {
+        'graph': graph,
+        'price': price,
         'error': display_error_message,
-
     }
     # Render products/main.html template with an empty context
     return render(request, 'products/main.html', context)
